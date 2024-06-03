1df0c66f74582813791ef9268bebc68400ce5780
Christian-Jordy05 <yordymar224@gmail.com>
2024-06-03 14:40:51 -0600

    prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // Inserte el código aquí
 let botonAgrega = document.getElementById("boton")
 let contador = document.getElementById("contador")
-let conteinerDeLasTareas = document.getAnimations("conteinerDeLasTareas")
+let conteinerDeLasTareas = document.getElementById("conteinerDeLasTareas")
+let inputAgre = document.getElementById("inputAgre")
+let texCon = document.getElementById("texCon")
+let texAlert = document.getElementById("texAlert")
 
+//funcion para agregar los div de tarea
+botonAgrega.addEventListener("click" , function(){
+    if (inputAgre.value === "")  {
+        /*aqui por si el usurio el da ingresar tarea y si no tiene texto le manda un alert 
+        diciendo que no se puede dejar blanco*/
+        let texAlert2 = document.createElement("h2")
+        texAlert2 = "no se puede dejar sin texto"
+        console.log(texAlert2);
+        
+    } else {
+      
 
+        // let tarea = document.createElement("div")
+        // tarea.style.backgroundColor = "red"
+        // let hola = conteinerDeLasTareas.appendChild(tarea)
+        // console.log(hola);
+    }
+})
